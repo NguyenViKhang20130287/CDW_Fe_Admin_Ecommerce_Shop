f277440a28705b28eec931e2a27a8bf1503325be
NguyenHuyHiep-20130258 <20130258@st.hcmuaf.edu.vn>
2024-04-09 23:39:08 +0700

    add create, edit category

---
+++ src/components/category/CategoryEdit.tsx
@@ -0,0 +1,32 @@
+import {
+    Edit,
+    SimpleForm,
+    TextInput,
+    required, useGetList, SelectArrayInputProps, BooleanInput
+} from 'react-admin';
+import React, {useEffect, useState} from "react";
+import {Category} from "../../types";
+
+export const CategoryEdit = (props: SelectArrayInputProps) => {
+    const [categories, setCategories] = useState<Category[]>([]);
+    const {data}: any = useGetList<Category>('category', {
+        pagination: {page: 1, perPage: 100},
+        sort: {field: 'name', order: 'ASC'},
+    });
+
+    useEffect(() => {
+        if (data) {
+            setCategories(data);
+        }
+    }, [data]);
+    return (
+        <Edit>
+            <SimpleForm>
+                <TextInput disabled label="Id" source="id"/>
+                <TextInput source="name" validate={required()}/>
+                <BooleanInput  source="status" label="Trạng thái" defaultValue={false}/>
+
+            </SimpleForm>
+        </Edit>
+    )
+};
